9e01ce7e49c3302f6c7853fdf85c39e8bb36bd56
github username <github email address>
2018-12-12 09:18:28 -0500

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,59 @@
+require "pry"
+# cart = [
+#   {"AVOCADO" => {:price => 3.0, :clearance => true }},
+#   {"AVOCADO" => {:price => 3.0, :clearance => true }},
+#   {"KALE"    => {:price => 3.0, :clearance => false}}
+# ]
+
+# cart = {
+#   "AVOCADO" => {:price => 3.0, :clearance => true, :count => 3},
+#   "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+# }
+#
+# coupons = {:item => "AVOCADO", :num => 2, :cost => 5.0}
+
+
+
 def consolidate_cart(cart)
-  # code here
+  cart.each_with_object({}) do |item, hash|
+    item.each do |type, attribute|
+      if hash[type]
+        attribute[:count] += 1
+      else
+        attribute[:count] = 1
+        hash[type] = attribute
+      end
+    end
+  end
 end
 
+# def consolidate_cart(cart)
+#   cart.each_with_object({}) do |item, hash|
+#     hash[item]
+# end
+
+
+# consolidate_cart(cart)
+
+# binding.pry
+
 def apply_coupons(cart, coupons)
-  # code here
+  coupon_cart_syntax = {}
+  cart.each do |items, parameters|
+    coupons.each do |coupon, data|
+      # binding.pry
+    if items == coupon[:item] && parameters[:count] >= coupons[:num]
+      parameters[:count] -= coupons[:num]
+        cart["#{items}" + " W/COUPON"] = {:price => coupons[:num], :clearance => parameters[:clearance], :count => 1}
+      end
+    end
+  end
 end
+
+
+# apply_coupons(cart, coupons)
+
+
 
 def apply_clearance(cart)
   # code here
